2255be55f5c1ceb9a6fc15ff1bbfba0b08316717
woojin <ehdwlsdnwls@naver.com>
2015-11-16 21:38:22 +0900

    add filereader and music class's function

---
+++ TeamProject/src/test.java
@@ -0,0 +1,10 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		MusicFile a = new MusicFile();
+		MusicFile b = new MusicFile("바람","나얼","D:\\기타기타기타\\Music\\덕후의 기운이 감돌고 있습니다\\Bremen");
+		
+	}
+
+}
