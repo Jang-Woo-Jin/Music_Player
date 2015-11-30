4e5e70fea3f1cba7b6e5c3d1ed005af97f66d3d4
starl1ght <alstmd3@naver.com>
2015-12-01 03:26:47 +0900

    GUI_MusicList Update

--- OOPFinal/src/GUI_MusicList.java
+++ OOPFinal/src/GUI_MusicList.java
@@ -1,20 +1,25 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.io.File;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 public class GUI_MusicList implements ListSelectionListener {
-	private JPanel musicListPanel = new JPanel(new GridLayout(MusicFile.getMusicFileNum(), 1)); 
+	private JPanel musicListPanel = new JPanel(new GridLayout()); 
 	private JList<MusicFile> musicList;
 	private DefaultListModel<MusicFile> listModel = new DefaultListModel<MusicFile>();
-
+	
 	public JPanel createListPanel() {
+		//TEST
 		for(int i = 0 ; i < 100 ; i++) {
 			listModel.addElement(new MusicFile("A"));
 			listModel.get(i).setName(Integer.toString(i));
 		}
+		//
 		musicList = new JList<MusicFile>(listModel);
 		musicList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		musicList.addListSelectionListener(this);
@@ -34,4 +39,9 @@ public class GUI_MusicList implements ListSelectionListener {
 	    System.out.println(list.getSelectedValue().getName());
         
 	}
+
+	 
+
 }
+
+
