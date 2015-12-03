9bc100948cb07df47798407b1e7c610c95ae12b6
woojin <ehdwlsdnwls@naver.com>
2015-12-04 01:27:29 +0900

    edit Alarm & Shutdown

--- src/main/java/Alarm/AutomaticShutdown.java
+++ src/main/java/Alarm/AutomaticShutdown.java
@@ -1,26 +1,32 @@
 package Alarm;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class AutomaticShutdown extends Thread {
 
-    public boolean letShutdown(String time) {
+   private long ringring;
+   
+   @Override
+   public void run(){
+      try {
+         Thread.sleep(ringring);
+         System.exit(0);
+      } 
+      catch (InterruptedException e) {
+         e.printStackTrace();
+      }
+   }
+   
+   public void setShutdown(String time){
 
-        int setTime = Integer.parseInt(time);
-        long setMilliTime = setTime * 1000 * 60;
-
-        Calendar currentTime = Calendar.getInstance();
-        Calendar shutTime = Calendar.getInstance();
-
-        shutTime.setTimeInMillis(currentTime.getTimeInMillis() + setMilliTime);
-
-        try {
-            Thread.sleep(shutTime.getTimeInMillis() - currentTime.getTimeInMillis());
-        } catch (InterruptedException e) {
-            return false;
-        }
-
-
-        return true;
-    }
+      int setTime = Integer.parseInt(time);
+      long setMilliTime = setTime * 1000 * 60;
+      
+      Calendar currentTime = Calendar.getInstance();
+      Calendar shutTime = Calendar.getInstance();
+      
+      shutTime.setTimeInMillis(currentTime.getTimeInMillis() + setMilliTime);
+   
+      ringring = shutTime.getTimeInMillis() - currentTime.getTimeInMillis();
+   }
 }
