902ccb006d3a359284cecdeb7c07435a2799ff4b
lyuha <beingfunnylife@gmail.com>
2015-12-04 06:54:33 +0900

    add directory seperator field, and remove unnecessary arguments

--- src/main/java/FileIO/FilePathParser.java
+++ src/main/java/FileIO/FilePathParser.java
@@ -1,16 +1,30 @@
 package FileIO;
 
-class FilePathParser {
+import OS.OSValidator;
+
+public class FilePathParser {
+    private static String directorySeperator;
+    static {
+        if (OSValidator.isWindows()) {
+            directorySeperator = new String("\\");
+        } else if (OSValidator.isPOSIX()) {
+            directorySeperator = new String("/");
+        }
+    }
+
+    public static String getDirectorySeperator() {
+        return directorySeperator;
+    }
 
     public static String getExtension(String fileStr) {
-        return fileStr.substring(fileStr.lastIndexOf(".") + 1, fileStr.length());
+        return fileStr.substring(fileStr.lastIndexOf(".") + 1);
     }
 
     public static String getPath(String fileStr) {
-        return fileStr.substring(0, fileStr.lastIndexOf("\\"));
+        return fileStr.substring(0, fileStr.lastIndexOf(directorySeperator));
     }
 
     public static String getFileName(String fileStr) {
-        return fileStr.substring(fileStr.lastIndexOf("\\") + 1, fileStr.lastIndexOf("."));
+        return fileStr.substring(fileStr.lastIndexOf(directorySeperator) + 1, fileStr.lastIndexOf("."));
     }
 }
