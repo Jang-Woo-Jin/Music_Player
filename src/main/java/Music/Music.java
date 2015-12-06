ae3c0536d1904855b8b55b63e9888fe41f727ece
woojin <ehdwlsdnwls@naver.com>
2015-12-06 21:47:58 +0900

    pull

--- src/main/java/Music/Music.java
+++ src/main/java/Music/Music.java
@@ -1,5 +1,6 @@
 package Music;
 
+import FileIO.FileIO;
 import FileIO.FilePathParser;
 
 import com.googlecode.mp4parser.util.Path;
@@ -7,6 +8,7 @@ import com.mpatric.mp3agic.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Music extends Mp3File {
     private final String FILE_INFO_ADDRESS = System.getProperty("user.home")
@@ -54,11 +56,33 @@ public class Music extends Mp3File {
 
     public Music(File file) throws UnsupportedTagException, InvalidDataException, IOException{
     	super(file.getAbsolutePath());
+    	boolean check = true;
     	String path = file.getAbsolutePath();
     	this.fileName = FilePathParser.getFileName(path);
     	this.fileAddress = FilePathParser.getPath(path);
-    	
+    	ArrayList<String> informationString = FileIO.readTextFile(FILE_INFO_ADDRESS, FILE_INFO_NAME);
+        String[] information = new String[5];
+
+        for (String iter : informationString) {
+            information = iter.split("/");
+            if (information[1].equals(fileName)) {
+            	 this.playCount = Integer.parseInt(information[0]);
+                 this.fileName = information[1];
+                 this.fileAddress = information[2];
+                 this.lyricsFileName = information[3];
+                 this.lyricsFileAddress = information[4];
+            	break;
+            }
+        }
+        if(check){
+        	this.playCount = 0;
+        	this.lyricsFileName = "null";
+        	this.lyricsFileAddress = "null";
+        }
+        
+    		
     }
+    
     private void setMusicInformation() {
         if (isV1Tag) {
             this.artist = id3v1Tag.getArtist();
@@ -73,8 +97,6 @@ public class Music extends Mp3File {
             this.album = id3v2Tag.getAlbum();
             this.image = id3v2Tag.getAlbumImage();
         }
-        for(int i=0;i<image.length;i++)
-        	System.out.println(this.image[i]);
     }
 
     public String getSaveInfo() {
