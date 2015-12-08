1caec4db86f20af8cfcddabc1a638daa43aa6a0a
starl1ght <alstmd3@naver.com>
2015-12-08 17:04:44 +0900

    Merge branch 'master' of github.com:Jang-Woo-Jin/OOP_final
    # Conflicts:
    #	src/main/java/FileIO/FilePathParser.java
    
    inspect redundable conde

--- src/main/java/Music/Lyric_Parser.java
+++ src/main/java/Music/Lyric_Parser.java
@@ -19,60 +19,29 @@ class Lyric_Parser {
         return time;
     }
 
-    public void setTime(int[][] time) {
-        this.time = time;
-    }
-
     private void splitLrc() {
         ArrayList<String> b = FileIO.readTextFile("C:", "test");
 
 
         for (int i = 0; i < b.size(); i++) {
-        	//System.out.println(b);
-            index = b.get(i).split("]");
+        	index = b.get(i).split("]");
             lrcTime[i] = index[0];
             lrcTime[i] = lrcTime[i].replaceAll("\\[", "");
-
             try {
                 lrc[i] = index[1];
             } catch (Exception ArrayIndexOutOfBoundsException) {
 
             }
-            
-           // System.out.println(i+"--"+lrc[i]);
-            
             time[i][0] = (lrcTime[i].charAt(0) - 48) * 10
                     + (lrcTime[i].charAt(1) - 48);
             time[i][1] = (lrcTime[i].charAt(3) - 48) * 10
                     + (lrcTime[i].charAt(4) - 48);
             time[i][2] = (lrcTime[i].charAt(6) - 48) * 10
                     + (lrcTime[i].charAt(7) - 48);
-
         }
 
     }
-
-    public String[] getLrcTime() {
-        return lrcTime;
-    }
-
-    public void setLrcTime(String[] lrcTime) {
-        this.lrcTime = lrcTime;
-    }
-
     public String[] getLrc() {
         return lrc;
-    }
-
-    public void setLrc(String[] lrc) {
-        this.lrc = lrc;
-    }
-
-    public String[] getIndex() {
-        return index;
-    }
-
-    public void setIndex(String[] index) {
-        this.index = index;
     }
 }
